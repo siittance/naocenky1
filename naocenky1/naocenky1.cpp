97de00afec696c5a92d73ee36f04f38f238fdab7
siittance <144900566+siittance@users.noreply.github.com>
2024-02-03 10:47:22 +0300

    Добавьте файлы проекта.

---
+++ naocenky1/naocenky1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+#pragma execution_character_set("utf-8")
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    int a = 4;
+    int b = 12;
+
+    int* yk1 = &a;
+    int* yk2 = &b;
+
+    *yk1 *= 3;
+    *yk2 /= 3;
+
+    std::cout << "Новое значение a: " << a << std::endl;
+    std::cout << "Новое значение b: " << b << std::endl;
+
+    return 0;
+}
+
+
